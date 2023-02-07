12ad61632674e1cb28c60d28dce373b387ab73fc
skirataar <95690923+skirataar@users.noreply.github.com>
2023-02-07 09:36:41 +0530

    Update header_CLL.c

--- header_CLL.c
+++ header_CLL.c
@@ -24,6 +24,21 @@ void insert_front(NODE head,int ele){
     head->link=temp;  
 }
 
+void insert_rear(NODE head, int ele){
+    NODE temp,cur=head;
+    temp=getnode();
+    temp->info=ele;
+    if(head==head->link)
+        head->link=temp;
+    else {
+    	while(cur->link!=head)  	
+    		cur=cur->link;
+    	cur->link=temp;
+    }
+    temp->link=head;  
+		  	        
+}
+
 void display(NODE head){
     NODE cur=head->link;
     if(head==head->link){
@@ -33,6 +48,20 @@ void display(NODE head){
     while(cur!=head){
         printf("%d ",cur->info);
         cur=cur->link;
+    }
+}
+
+void delete_front(NODE head){
+    NODE first;
+    
+    if(head==head->link){
+        printf("No elements");
+    }
+    else{
+        first=head->link;
+        printf("\nDeleted element: %d\n",first->info);
+        head->link=first->link;
+        free(first);
     }
 }
 
@@ -54,10 +83,29 @@ void delete_rear(NODE head){
 }
 
 void main(){
-    NODE head = getnode();
-    head->link = head;
-    insert_front(head,22);
-    insert_front(head,33);
-    insert_front(head,44);
-    display(head);
+    NODE head=getnode();
+    head->link=head;
+    int ele,ch;
+    while(1){
+        printf("\n1:Insert Front\n2:Insert Rear\n3:Delete Front\n4:Delete Rear\n5:Display\n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: printf("Enter element: ");
+                    scanf("%d",&ele);
+                    insert_front(head,ele);
+                    break;
+            case 2: printf("Enter element: ");
+                    scanf("%d",&ele);
+                    insert_rear(head,ele);
+                    break;
+            case 3: delete_front(head);
+                    break;
+            case 4: delete_rear(head);
+                    break;
+            case 5: display(head);
+                    break;
+            default: exit(0);
+
+        }
+    }
 }
